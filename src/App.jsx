56e058384596f45e368244bdd217ddbc7fc9c09c
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-22 14:37:59 +0100

    another problem with files solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header.jsx";
 import HelloSection from "./components/HelloSection/HelloSection.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import ContactSection from "./components/ContactSection/ContactSection.jsx";
-import StackSection from "./components/StackSection/.jsx";
+import StackSection from "./components/StackSection/StackSection.jsx";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection.jsx";
 
 function App() {
