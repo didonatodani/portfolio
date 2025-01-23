030c0820cfaf004aa41ae4706ae15e6cb9d46bd5
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-23 14:41:17 +0100

    contact section styled

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,32 +1,37 @@
-import "./ContactSection.css"
+import "./ContactSection.css";
 
 function ContactSection() {
   return (
     <section id="contact-section">
-      <p className="contact-description">Hey there! It's Dani here, a creative web dev passionate about building efficient and user-friendly applications. Let's build something great!</p>
-      <a href="mailto:didonatovdaniela@gmail.com">email me</a>
-      <a
-        href="https://www.linkedin.com/in/dani-di-donato-web-dev/"
-        target="_blank"
-        className="contact-link"
-      >
-        Linkedin
-      </a>
-      <a
-        href="https://github.com/didonatodani/"
-        target="_blank"
-        className="contact-link"
-      >
-        GitHub
-      </a>
-      <a
-        href="../../public\DDDV.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="contact-link"
-      >
-        <button className="contact-btn">Resume</button>
-      </a>
+      <p className="contact-description">
+        Hey there! It's Dani here, a creative web dev passionate about building
+        efficient and user-friendly applications. <span id="description-span">Let's build something great!</span>
+      </p>
+      <div className="contact-section-links">
+        <a href="mailto:didonatovdaniela@gmail.com">Email me</a>
+        <a
+          href="https://www.linkedin.com/in/dani-di-donato-web-dev/"
+          target="_blank"
+          className="contact-link"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/didonatodani/"
+          target="_blank"
+          className="contact-link"
+        >
+          GitHub
+        </a>
+        <a
+          href="../../public\DDDV.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-link"
+        >
+          Resume
+        </a>
+      </div>
     </section>
   );
 }
