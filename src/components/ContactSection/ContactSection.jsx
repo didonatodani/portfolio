c22b89c0404b4b27de10c39e2c458ea7401011d9
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-23 13:59:15 +0100

    fixing paddings

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,6 +1,9 @@
+import "./ContactSection.css"
+
 function ContactSection() {
   return (
     <section id="contact-section">
+      <p className="contact-description">Hey there! It's Dani here, a creative web dev passionate about building efficient and user-friendly applications. Let's build something great!</p>
       <a href="mailto:didonatovdaniela@gmail.com">email me</a>
       <a
         href="https://www.linkedin.com/in/dani-di-donato-web-dev/"
