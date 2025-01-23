71db83d28ae4acd6fef8102e419602a9553f2632
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-23 12:55:53 +0100

    home section's styles defined and call to action btn added

--- src/components/HelloSection/HelloSection.jsx
+++ src/components/HelloSection/HelloSection.jsx
@@ -6,9 +6,7 @@ function HelloSection() {
       <h2>Dani Di Donato</h2>
       {/* this h1 will have animations in the future */}
       <h1>&lt;Web Dev/&gt;</h1>
-
-      {/* better redirect to projects section */}
-      <button>VIEW MY WORK!</button>
+      <a href="#projects-section" className="hello-btn">VIEW MY WORK!</a>
     </section>
   );
 }
