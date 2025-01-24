d5f241af3a8c23871ad475b5aaecb7b5195101fa
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-24 20:08:54 +0100

    active classes added to links on sidebar, paddings fixed on sections

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,15 @@
 import "./Sidebar.css";
 import CloseIcon from "../../assets/close.svg";
+import { useState } from "react";
 
 function Sidebar({ showCloseButton, closeMenu }) {
+  const [activeLink, setActiveLink] = useState("");
+
+  function handleClick(sectionId) {
+    setActiveLink(sectionId);
+    closeMenu();
+  }
+
   return (
     <aside>
       {showCloseButton && (
@@ -20,33 +28,37 @@ function Sidebar({ showCloseButton, closeMenu }) {
           href="#hello-section"
           onClick={() => {
             closeMenu();
+            handleClick("hello-section");
           }}
         >
-          <span className="ref-span">Hi!</span>
+          <span className={`ref-span ${activeLink === "hello-section" ? "active" : ""}`}>Hi!</span>
         </a>
         <a
           href="#contact-section"
           onClick={() => {
             closeMenu();
+            handleClick("contact-section");
           }}
         >
-          <span className="ref-span">Contact</span>
+          <span className={`ref-span ${activeLink === "contact-section" ? "active" : ""}`}>Contact</span>
         </a>
         <a
           href="#stack-section"
           onClick={() => {
             closeMenu();
+            handleClick("stack-section");
           }}
         >
-          <span className="ref-span">Tech Stack</span>
+          <span className={`ref-span ${activeLink === "stack-section" ? "active" : ""}`}>Tech Stack</span>
         </a>
         <a
           href="#projects-section"
           onClick={() => {
             closeMenu();
+            handleClick("projects-section");
           }}
         >
-          <span className="ref-span">Projects</span>
+          <span className={`ref-span ${activeLink === "projects-section" ? "active" : ""}`}>Projects</span>
         </a>
       </div>
     </aside>
