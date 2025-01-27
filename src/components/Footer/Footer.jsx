d0d2b7b59a08ba261f5114a0110c02ef5e03aef9
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-27 12:53:09 +0100

    favicon added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,15 @@ function Footer() {
   return (
     <footer>
       <small>Dani Di Donato © 2025 | Barcelona</small>
-      <div className="contact-footer-div">
-        <a href="https://www.linkedin.com/in/dani-di-donato-web-dev/">
-          <img src={inIcon} alt="linkedin icon" />
+      <div className="contact-footer-div" target="_blank">
+        <a href="https://www.linkedin.com/in/dani-di-donato-web-dev/" target="_blank">
+          <img src={inIcon} alt="linkedin icon" className="icon"/>
         </a>
-        <a href="https://github.com/didonatodani/">
-          <img src={githubIcon} alt="github icon" />
+        <a href="https://github.com/didonatodani/" target="_blank">
+          <img src={githubIcon} alt="github icon"className="icon"/>
         </a>
-        <a href="mailto:didonatovdaniela@gmail.com">
-          <img src={emailIcon} alt="email icon" />
+        <a href="mailto:didonatovdaniela@gmail.com" target="_blank">
+          <img src={emailIcon} alt="email icon" className="icon" />
         </a>
       </div>
     </footer>
